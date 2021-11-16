a79254e919ef526f29b1ab22b8a244ebcc30007f
eric <deskagebunshin@gmail.com>
2021-11-16 17:53:07 +0000

    added debug mode to game controller

--- Assets/_Scripts/GameController.cs
+++ Assets/_Scripts/GameController.cs
@@ -4,18 +4,47 @@ using UnityEngine;
 
 public class GameController : StateMachine
 {
-
+    public bool debugMode = false;
+    public state debugState;
     public UIManager UI;
     // Start is called before the first frame update
     void Start()
     {
         UI = GetComponent<UIManager>();
-        SetState(new WordSelect(this));
+
+        if (!debugMode)
+        {
+            SetState(new WordSelect(this));
+        } else
+        {
+            switch (debugState)
+            {
+                case state.SelectWord:
+
+                    SetState(new WordSelect(this));
+                    break;
+
+                case state.CrossHair:
+
+                    SetState(new Crosshair(this));
+                    break;
+
+                case state.SpellWord:
+
+                    SetState(new SpellWord(this));
+                    break;
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public enum state
+    {
+        SelectWord, CrossHair, SpellWord
     }
 }
